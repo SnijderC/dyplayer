ace58f9425ebb1638a391b3229a7e436754d527d
Chris <chris@chrissnijder.nl>
2020-01-29 23:54:49 +0100

    Update esp32 example and try to improve platformio.ini

--- examples/esp32/PlaySounds.cpp
+++ examples/esp32/PlaySounds.cpp
@@ -1,7 +1,10 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include "driver/uart.h"
-#include <DYPlayerESP32.h>
+#include <esp_log.h>
+#include <driver/uart.h>
+#include "DYPlayerESP32.h"
+
+#define TAG "dyplayer"
 
 // Initialise the player on uart2, pins TX: 18, RX: 19.
 DY::Player player(UART_NUM_2, 18, 19);
@@ -9,11 +12,12 @@ DY::Player player(UART_NUM_2, 18, 19);
 extern "C" void app_main(void) {
   player.setVolume(15); // 50% Volume
   while(true) {
-    // Try to play sound: 00001.MP3, 00002.MP3, 00003.MP3, 00004.MP3, 00005.MP3
+    ESP_LOGI(TAG, "Player device %u", player.getDevice());
     // Change to next sound after 5 seconds (non-blocking call so will cut off
     // currently playing sound if any).
-    for (uint8_t i = 1; i < 10; i++) {
+    for (uint8_t i = 1; i < player.soundCount(); i++) {
       player.playSpecified(i);
+      ESP_LOGI(TAG, "Playing song %d", player.getPlayingSound());
       vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
   }
